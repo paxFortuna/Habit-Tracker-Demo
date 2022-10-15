ab9aebb718c06eeb18746df91d851b2d37100a51
fortuna <avidia38@gmail.com>
2022-10-15 18:12:51 +0900

    feat: all code

--- lib/components/habit_tile.dart
+++ lib/components/habit_tile.dart
@@ -2,10 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 class HabitTile extends StatelessWidget {
-
   final String habitName;
+  final VoidCallback onTimeTap;
+  final VoidCallback settingsTapped;
+  final int timeSpent;
+  final int timeGoal;
+  final bool habitStarted;
 
-  const HabitTile({Key? key, required this.habitName}) : super(key: key);
+  const HabitTile({
+    Key? key,
+    required this.habitName,
+    required this.onTimeTap,
+    required this.settingsTapped,
+    required this.timeSpent,
+    required this.timeGoal,
+    required this.habitStarted,
+  }) : super(key: key);
+
+  // convert seconds into min: sec -> e.g 62 seconds = 1:02 min
+  String formatToMinSec(int totalSeconds) {
+    String secs = (totalSeconds % 60).toString();
+    String mins = (totalSeconds / 60).toStringAsFixed(1);
+    // 59 sceonds -> 0:59
+    // (59/60).toStringAsFixed(1) = 0.9.. = 1
+
+    // if secs is a 1 digit number, place a 0 infront of it
+    if (secs.length == 1) {
+      secs = '0$secs';
+    }
+    // if mins is a 1 gigit number
+    if (mins[1] == '.') {
+      mins = mins.substring(0, 1);
+      // 0.98 min -> 0 mins
+    }
+    return mins + ':' + secs;
+  }
+
+  // calculatet progerss pecentage
+  double percentageCompleted() {
+    return timeSpent / (timeGoal * 60);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,20 +58,34 @@ class HabitTile extends StatelessWidget {
           children: [
             Row(
               children: [
-               Container(
-                 height: 60,
-                 width: 60,
-                 child: Stack(
-                   children: [
-                     // progress circle
-                     CircularPercentIndicator(
-                       radius: 30,
-                     ),
-                     // play pause button
-                     Center(child: Icon(Icons.play_arrow)),
-                   ],
-                 ),
-               ),
+                GestureDetector(
+                  onTap: onTimeTap,
+                  child: SizedBox(
+                    height: 60,
+                    width: 60,
+                    child: Stack(
+                      children: [
+                        // progress circle
+                        CircularPercentIndicator(
+                          radius: 30,
+                          percent: percentageCompleted() < 1
+                              ? percentageCompleted()
+                              : 1,
+                          progressColor: percentageCompleted() > 0.5
+                              ? (percentageCompleted() > 0.75
+                                  ? Colors.green
+                                  : Colors.orange)
+                              : Colors.red,
+                        ),
+                        // play pause button
+                        Center(
+                          child: Icon(
+                              habitStarted ? Icons.pause : Icons.play_arrow),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
                 const SizedBox(width: 20),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -43,22 +93,26 @@ class HabitTile extends StatelessWidget {
                     // habit name
                     Text(
                       habitName,
-                      style: TextStyle(
+                      style: const TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 18,
                       ),
                     ),
-                    SizedBox(height: 4),
+                    const SizedBox(height: 4),
                     //progress
                     Text(
-                      '2:00 / 10 = 20%',
-                      style: TextStyle(color: Colors.grey),
+                      '${formatToMinSec(timeSpent)} / ${formatToMinSec(timeGoal)}'
+                      ' = ${(percentageCompleted() * 100).toStringAsFixed(0)}%',
+                      style: const TextStyle(color: Colors.grey),
                     ),
                   ],
                 ),
               ],
             ),
-            const Icon(Icons.settings),
+            GestureDetector(
+              onTap: settingsTapped,
+              child: const Icon(Icons.settings),
+            ),
           ],
         ),
       ),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_koko/components/habit_tile.dart';
-
-import '../components/my_fab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,33 +12,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // overall habit summary
+  List<dynamic> habitList = [
+    // [ habitName, habitStarted, timeSpent(sec), timeGoal(min) ]
+    ['Exercise', false, 0, 1],
+    ['Read', false, 0, 5],
+    ['Meditate', false, 0, 10],
+    ['Code', false, 0, 20],
+  ];
+
+  void habitStarted(int index) {
+    // note what the start time is
+    var startTime = DateTime.now();
+
+    // include the time already elapsed
+    int elapsedTime = habitList[index][2];
+
+    // habit started or stopped
+    setState(() {
+      habitList[index][1] = !habitList[index][1];
+    });
+
+    if (habitList[index][1]) {
+      // keep the time going!
+      Timer.periodic(const Duration(seconds: 1), (timer) {
+        setState(() {
+          // check when the user has stopped the timer
+          if (!habitList[index][1]) {
+            timer.cancel();
+          }
+
+          // calculate the time elapsed by comparing current time and state time
+          var currentTime = DateTime.now();
+          habitList[index][2] = elapsedTime + currentTime.second -
+              startTime.second +
+              60 * (currentTime.minute - startTime.minute) +
+              60 * 60 * (currentTime.hour - startTime.hour);
+        });
+      });
+    }
+
+  }
+
+  void settingsOpend(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Setting for" + habitList[index][0]),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey[900],
         title: const Text('Habit Tracker'),
       ),
+      body: ListView.builder(
+        itemCount: habitList.length,
+        itemBuilder: (context, index) {
+          return HabitTile(
+            habitName: habitList[index][0],
+            onTimeTap: () {
+              habitStarted(index);
+            },
+            settingsTapped: () {
+              settingsOpend(index);
+            },
+            habitStarted: habitList[index][1],
+            timeSpent: habitList[index][2],
+            timeGoal: habitList[index][3],
+          );
+        },
+      ),
       backgroundColor: Colors.grey[300],
-      floatingActionButton: MyFloatingActionButton(
-        onPressed: () {},
-      ),
-      body: Column(
-        children: [
-          HabitTile(
-            habitName: 'Exercise',
-          ),
-          HabitTile(
-            habitName: 'Read',
-          ),
-          HabitTile(
-            habitName: 'Meditate',
-          ),
-          HabitTile(
-            habitName: 'Code',
-          ),
-        ],
-      ),
+      // floatingActionButton: MyFloatingActionButton(
+      //   onPressed: () {},
+      // ),
     );
   }
 }
